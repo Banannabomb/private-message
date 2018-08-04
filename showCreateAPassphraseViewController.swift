4d82b48cc377886da909283d7f76fc823abbb8fb
Banannabomb <36425629+Banannabomb@users.noreply.github.com>
2018-08-04 09:25:01 -0700

    Add files via upload

--- showCreateAPassphraseViewController.swift
+++ showCreateAPassphraseViewController.swift
@@ -12,7 +12,7 @@ var passphraseArray: [String] = []
 class showCreateAPassphraseViewController: UIViewController {
     
     @IBOutlet weak var passphraseTextView: UITextView!
-    //var passphraseArray: [String?] = []
+    var passphraseArray: [String?] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- showEnterAPassphraseViewController.swift
+++ showEnterAPassphraseViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 var enteredPassphraseInTextView: String = ""
 class showEnterAPassphraseViewController: UIViewController {
     
+
     @IBOutlet weak var passphraseEntryTextView: UITextView!
-    //var enteredPassphraseInTextView: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
